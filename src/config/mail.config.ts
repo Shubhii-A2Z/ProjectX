a1724161d3a303f3c7cf9d1082bc34d65fb29aea
unknown <negishubham2005@gmail.com>
2026-09-11 08:45:10 +0530

    mailer: Implement MailHog with Docker File for Email Testing

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -1,15 +1,22 @@
 import nodemailer from "nodemailer";
 import serverConfig from "./server.config";
 
-const transporter=nodemailer.createTransport({
+const transporter = serverConfig.NODE_ENV=="DEV"
+? nodemailer.createTransport({
+    host: process.env.SMTP_HOST || "localhost",
+    port: Number(process.env.SMTP_PORT) || 1025,
+    secure: false,
+    ignoreTLS: true,
+})
+: nodemailer.createTransport({
     service: "Gmail",
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-        user: serverConfig.MAIL_ID,
-        pass: serverConfig.APP_PASSWORD
-    }
+    user: serverConfig.MAIL_ID,
+    pass: serverConfig.APP_PASSWORD,
+    },
 });
 
 export default transporter;
--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -11,5 +11,6 @@ export default{
     REDIS_HOST: process.env.REDIS_HOST,
     REDIS_PORT: process.env.REDIS_PORT,
     RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
-    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET
+    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
+    NODE_ENV: process.env.NODE_ENV
 }
